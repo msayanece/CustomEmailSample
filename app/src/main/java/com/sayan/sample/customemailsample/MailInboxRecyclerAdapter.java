7114c5c52d7f71c4345d99235d6d3b464033c97f
Joy praksh <joyprakash.d@enoders.co.in>
2018-05-22 10:27:13 +0530

    ReadInbox designed modified

--- app/src/main/java/com/sayan/sample/customemailsample/MailInboxRecyclerAdapter.java
+++ app/src/main/java/com/sayan/sample/customemailsample/MailInboxRecyclerAdapter.java
@@ -1,19 +1,24 @@
 package com.sayan.sample.customemailsample;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Admin on 04-09-2017.
@@ -40,7 +45,18 @@ public class MailInboxRecyclerAdapter extends RecyclerView.Adapter<MailInboxRecy
         String from = inboxModel.getFrom();
 //        message.getMessageNumber();
         holder.mailFrom.setText(from);
-        holder.mailSubject.setText(subject);
+        holder.profileIcon.setText(from.substring(0,1));
+        holder.profileContainer.setColorFilter(getRandomColor());
+        if (subject.equalsIgnoreCase("")){
+            holder.mailSubject.setText("[no subject]");
+        }
+        else
+            holder.mailSubject.setText(subject);
+    }
+
+    private int getRandomColor() {
+        Random rnd = new Random();
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
     }
 
     @Override
@@ -51,13 +67,20 @@ public class MailInboxRecyclerAdapter extends RecyclerView.Adapter<MailInboxRecy
     class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView mailFrom;
         private final TextView mailSubject;
+        private  TextView profileIcon;
+        private TextView profileBackground;
+        private RelativeLayout iconContainer;
+        private CircleImageView profileContainer;
         LinearLayout view;
 
         ViewHolder(View itemView) {
             super(itemView);
             mailFrom = itemView.findViewById(R.id.mailFrom);
             mailSubject = itemView.findViewById(R.id.mailSubject);
-//            view = (LinearLayout) itemView;
+            profileIcon=itemView.findViewById(R.id.profile_icon);
+            //profileBackground=itemView.findViewById(R.id.profileIcon);
+            iconContainer=itemView.findViewById(R.id.icon_container);
+            profileContainer=itemView.findViewById(R.id.profile_container);
         }
     }
 }
